d22a364a1bc853f20808b70dea35c88a7604ccd3
华文伟 <00320899@shqpc00003008.want-want.com>
2017-08-14 10:12:21 +0800

    modify

---
+++ wow.js
@@ -0,0 +1,72 @@
+$(function(){
+	$(".htabs").tabs( );
+  var vm=new Vue({
+  	el:".hvue",
+  	data:{
+  		msg:"双向绑定"
+  	}
+  });
+
+
+// var a=1;
+// while(a<10){
+// 	alert(a);
+// 	a++;
+
+// }
+
+function hua(){
+var hua=["茄子","哪款","阿萨德","asd"];
+for(var h=0;h<hua.length;h++){
+	alert(hua[h])
+}
+
+
+};
+
+
+// hua();
+
+
+
+function hh(num1,num2){
+	var total=num1*num2;
+	return total;
+
+}; 
+
+// alert(hh(2,3))
+
+
+
+
+
+
+
+// var cym=11;
+// function a(){
+// 	 cym=12;
+	
+// };
+
+// alert(cym)
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ })
