a179e400758b80b5a80e9a25e838729196104ae3
华文伟 <00320899@shqpc00003008.want-want.com>
2017-11-21 14:33:40 +0800

    modify

--- wow.js
+++ wow.js
@@ -1,4 +1,60 @@
 $(function(){
+   var croc = Snap.select("#crocodile"),
+                head = croc.select("#upper-head"),
+                jaw = croc.select("#upper-jaw"),
+                symbol = croc.select("#symbol"),
+                timer;
+                
+            var pivots = [
+                [44, 147],
+                [92, 126]
+            ];
+
+            function close() {
+                clearTimeout(timer);
+
+                head.animate({ 
+                    transform: "r" + [8, pivots[0]]
+                }, 500, mina.backin);
+                
+                jaw.animate({
+                    transform: "r" + [37, pivots[1]]
+                }, 500, mina.backin);
+
+                timer = setTimeout(function () {
+                    symbol.animate({
+                        transform: "t-70,40r40"
+                    }, 100);
+                }, 400);
+            }
+
+            function open() {
+                clearTimeout(timer);
+
+                head.animate({ 
+                    transform: "r" + [0, pivots[0]]
+                }, 700, mina.elastic);
+                
+                jaw.animate({
+                    transform: "r" + [0, pivots[1]]
+                }, 700, mina.elastic);
+
+                symbol.animate({
+                    transform: "t0,0r0"
+                }, 500, mina.elastic);
+            }
+
+            timer = setTimeout(close, 50);
+
+            croc.hover(open, 
+                function () {
+                    timer = setTimeout(close, 200);
+                }
+            );
+
+
+
+
   //滚动触发函数scroll
   $(window).scroll(function(){
     var f=1000;
